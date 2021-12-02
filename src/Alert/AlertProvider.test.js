8a8c4934d98ca1dc667dd567259003daea221340
Namrata Ajmeri <namrataajmeri@Namratas-MacBook-Pro.local>
2021-12-02 09:33:13 -0500

    Added test cases and restored alert message design

--- src/Alert/AlertProvider.test.js
+++ src/Alert/AlertProvider.test.js
@@ -7,6 +7,9 @@ import {
 } from "@testing-library/react";
 import AlertProvider, { useAlert } from "./AlertProvider";
 import PropTypes from "prop-types";
+import { config } from "react-transition-group";
+
+config.disabled = true;
 
 const MockAlerts = ({ message }) => {
   const { addAlert, removeAlert } = useAlert();
@@ -42,7 +45,7 @@ describe("Alert Provider tests", () => {
 
   test("Alert messages should get displayed when alert button is clicked", async () => {
     const message = "Test Message";
-    const { queryByText, queryByLabelText, queryByRole } = render(
+    const { queryByText, queryByLabelText, queryByRole, getByRole } = render(
       <AlertProvider timeout={100}>
         <MockAlerts message={message} />
       </AlertProvider>
@@ -50,7 +53,7 @@ describe("Alert Provider tests", () => {
 
     const addAlertButton = queryByText("Add Alert");
     fireEvent.click(addAlertButton);
-
+    //expect(getByRole("alert")).toEqual(message);
     expect(queryByRole("alert").textContent).toEqual(message);
     expect(queryByLabelText("close")).toBeTruthy();
 
@@ -59,22 +62,39 @@ describe("Alert Provider tests", () => {
     expect(queryByRole("alert")).toBeFalsy();
   });
 
-  test("Alert message should get deleted when exceeds max alerts", async () => {
+  test("Remove alert", async () => {
     const message = "Test Message";
-    const { queryByText, queryAllByRole } = render(
+    const { queryByText, getAllByRole } = render(
       <AlertProvider timeout={100}>
         <MockAlerts message={message} />
       </AlertProvider>
     );
 
+    const removeAlertButton = queryByText("Remove Alert");
     const addAlertButton = queryByText("Add Alert");
     fireEvent.click(addAlertButton);
     fireEvent.click(addAlertButton);
     fireEvent.click(addAlertButton);
-    fireEvent.click(addAlertButton);
-    fireEvent.click(addAlertButton);
-    fireEvent.click(addAlertButton);
-    fireEvent.click(addAlertButton);
-    expect(queryAllByRole("alert").length).toEqual(5);
+    fireEvent.click(removeAlertButton);
+
+    expect(getAllByRole("alert").length).toEqual(2);
+  });
+
+  test("Alert message should get deleted when exceeds max alerts", async () => {
+    const message = "Test Message";
+    const maxAlerts = 3;
+    const { queryByText, queryAllByRole } = render(
+      <AlertProvider maxAlerts={maxAlerts}>
+        <MockAlerts message={message} />
+      </AlertProvider>
+    );
+
+    const addAlertButton = queryByText("Add Alert");
+
+    for (let i = 0; i < maxAlerts + 1; i++) {
+      fireEvent.click(addAlertButton);
+    }
+
+    expect(queryAllByRole("alert").length).toEqual(maxAlerts);
   });
 });
